7272b8d126798d7ab2484508798cf4128b540d7f
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2025-02-22 16:59:55 +0600

    database connection

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+// src/db/index.js
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionString = `${process.env.MONGODB_URL}/${DB_NAME}`;
+        console.log("Connecting with string:", connectionString); // Debug log
+        const connectionInstance = await mongoose.connect(connectionString);
+        console.log(
+            `\nConnected to MongoDB at ${connectionInstance.connection.host}:${connectionInstance.connection.port}/${connectionInstance.connection.name}\n`
+        );
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
